cc6073335bec57e10af11571d73b9bbd60ff6da3
axemasquelin <amasquelin@live.com>
2020-06-17 22:29:05 -0400

    Finished New Stat Function

--- src/statistics.py
+++ src/statistics.py
@@ -14,26 +14,23 @@ from architectures import *
 import preprocessing
 import utils
 
-import torchvision.transforms as transforms
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.nn as nn
-import torch.tensor
-import torchvision
-import torch
-
 import matplotlib.pyplot as plt
+import statsmodels as sm
+import pandas as pd
 import numpy as np
-import cv2, sys, os
+import scipy
+import cv2, sys, os, csv
 # ---------------------------------------------------------------------------- #
 
-def mutlitest_stats():
+def multitest_stats(df1, df2):
     """
     Definition:
     Inputs:
     Outputs:
     """
-    print('running analysis')
+    print(df1['aucs'])
+    t, p = scipy.stats.ttest_ind(df1['aucs'], df2['aucs'])
+    print(p)
 
 def violin_plots():
     """
@@ -54,8 +51,9 @@ if __name__ == '__main__':
     models = [
             'Wave1',                # Single Level Wavelet Decomposition Layer extracting 4 features
             'Conv1',                # Convolutional Layer 4 Feature Extracted
-            # 'inception_wave',       # Multi Level Wavelet Decomposition
-            # 'inception_conv',       # Multiscale Convolutional Module.
+            'inception',
+            'inception_wave',       # Multi Level Wavelet Decomposition
+            'inception_conv',       # Multiscale Convolutional Module.
             # 'AlexNet',              # Standard Alexnet Architecture with modified classifier
             # 'WalexNet',             # Wavelet Alexnet Architecture
             ]
@@ -73,44 +71,45 @@ if __name__ == '__main__':
 
     # Dataframe Inits_
     df = pd.DataFrame()              # General Dataframe to generate Bar-graph data
-    df_alex = pd.DataFrame()         # AlexeNet Dataframe for violin plots
-    df_walex = pd.DataFrame()        # WalexNet Dataframe for violin plots
     df_conv1 = pd.DataFrame()        # Conv Layer Dataframe for violin plots
     df_wave1 = pd.DataFrame()        # Wavelet Layer Dataframe for violin plots
+    df_wavecept = pd.DataFrame()     # Multi-level Wavelet Dataframe
+    df_convcept = pd.DataFrame()     # Multi-level Convolutional Dataframe
 
-    for root, dirs, files in os.walk(os.getcwd()[0] + "/results/", topdown = True):
+    print(os.path.split(os.getcwd()))
+
+    for root, dirs, files in os.walk(os.path.split(os.getcwd())[0] + "/results/", topdown = True):
         for name in files:
-            if (name.endswith("aucs.csv")):
+            if (name.endswith(metrics[0] + ".csv")):
                 header = name.split('_')[0]
-                print(name.split('_')[0])
-                headers.append(header)
-                mean_ = []
-                filename = os.path.join(root,name)
-                with open(filename, 'r') as f:
-                    reader = csv.reader(f)
-                    next(reader)
-                    for row in reader:
-                        for l in range(len(row)-1):
-                            mean_.append(float(row[l+1]))
-                df[header] = np.transpose(mean_)
-                if (name.split('_')[0] == 'Conv1'):
-                    df_conv1[name.split('_')[0]] = np.transpose(mean_)
-                if (name.split('_')[0] == 'Wave1'):
-                    df_wave1[name.split('_')[0]] = np.transpose(mean_)
-                if (name.split('_')[0] == 'inception'):
-                    if(name.split('_')[1] == 'conv'):
-                        df_convcept[name.split('_')[1]] = np.transpose(mean_)
-                    if(name.split('_')[1] == 'wave'):
-                        df_wavecept[name.split('_')[1]] = np.transpose(mean_)
+                if header in models:
+                    mean_ = []
+                    filename = os.path.join(root,name)
+                    with open(filename, 'r') as f:
+                        reader = csv.reader(f)
+                        next(reader)
+                        for row in reader:
+                            for l in range(len(row)-1):
+                                mean_.append(float(row[l+1]))
+                    df[header] = np.transpose(mean_)
+                    if (name.split('_')[0] == 'Conv1'):
+                        df_conv1[metrics[0]] = np.transpose(mean_)
+                    if (name.split('_')[0] == 'Wave1'):
+                        df_wave1[metrics[0]] = np.transpose(mean_)
+                    if (name.split('_')[0] == 'inception'):
+                        if(name.split('_')[1] == 'conv'):
+                            df_convcept[metrics[0]] = np.transpose(mean_)
+                        if(name.split('_')[1] == 'wave'):
+                            df_wavecept[metrics[0]] = np.transpose(mean_)
 
-            if check_stats:
-                print("Comparing Single-level Analysis")
-                multitest_stats(df_wave1, df_conv1)
+    if check_stats:
+        print("Comparing Single-level Analysis")
+        multitest_stats(df_wave1, df_conv1)
+        
+        print("Comparing Multi-level Analysis")
+        multitest_stats(df_wavecept, df_convcept)
             
-                print("Comparing Multi-level Analysis")
-                multitest_stats(df_wavecept, df_convcept)
-            
-            if create_violin:
-                print("Violin Plots")
+    if create_violin:
+        print("Violin Plots")
 
                         
